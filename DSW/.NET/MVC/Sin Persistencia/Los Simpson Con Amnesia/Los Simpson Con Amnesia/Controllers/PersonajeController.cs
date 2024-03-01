77a5a7e84900fc912cdd568b3085a15f9035cde3
Aday <adaymarfil89@gmail.com>
2024-03-01 15:58:47 +0200

    Sin Persistencia

---
+++ DSW/.NET/MVC/Sin Persistencia/Los Simpson Con Amnesia/Los Simpson Con Amnesia/Controllers/PersonajeController.cs
@@ -0,0 +1,92 @@
+﻿using Los_Simpson_Con_Amnesia.Models;
+using Los_Simpson_Con_Amnesia.Views.Servicios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Los_Simpson_Con_Amnesia.Controllers
+{
+    //Se encarga de ofrecer rutas (endpoints)
+    public class PersonajeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //GET -> Ir al formulario
+        [HttpGet]
+        public IActionResult Create()
+        {
+            //Forma de comunicación entre Vista y Controlador 1:
+            //modelo [Model] -> (dentro del View() poner un objeto)
+            return View(new Personaje());
+        }
+        
+        //POST -> Tramita info del formulario
+        [HttpPost]
+        public IActionResult Create(Personaje createPersonaje)
+        {
+            //Si hay algún error, vuelve a la página y muestra los errores
+            //Si no hay  errores, crea el personaje y va hacia el listado (para ver los cambios)
+            if (ModelState.IsValid)
+            {
+                PersonajeService.CreateCharacter(createPersonaje);
+
+                return RedirectToAction("ReadAll");
+            }
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult ReadAll()
+        {
+            //Forma 2: uso del ViewBag/ViewData[""]
+            ViewBag.listaPersonaje = PersonajeService.ListCharacter();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ReadSingle(int id)
+        {
+            Personaje readChar = PersonajeService.SearchCharacter(id);
+            return View(readChar);
+        }
+
+
+        [HttpGet]
+        public IActionResult Update(int idUpdate)
+        {
+            return View(PersonajeService.GetPersonaje(idUpdate));
+        }
+
+        [HttpPost]
+        public IActionResult Update(Personaje updatePersonaje)
+        {
+            if (ModelState.IsValid)
+            {
+                PersonajeService.UpdateCharacter(updatePersonaje);
+                return RedirectToAction("ReadAll");
+            }
+
+            return View(updatePersonaje);
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int idDelete)
+        {
+            PersonajeService.DeleteCharacter(idDelete);
+            return RedirectToAction("ReadAll");
+        }
+
+    }
+}
---
+++ DSW/.NET/MVC/Sin Persistencia/Los Simpson Con Amnesia/Los Simpson Con Amnesia/Models/Personaje.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Los_Simpson_Con_Amnesia.Models
+{
+    public class Personaje
+    {
+        [HiddenInput(DisplayValue = false)] /* Oculta el campo en los formularios */
+        public int Id { get; set; }
+
+        [DisplayName("Nombre")] /* Muestra el nombre personalizado */
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "El NOMBRE debe tener entre 3 y 15 caractéres...")]
+        public string Name { get; set; }
+
+        [DisplayName("Edad")]
+        [Required(ErrorMessage = "Campo Obligatorio")] /* Indica que el campo es requerido (sorpresa) */
+        [Range(0, 100, ErrorMessage = "La EDAD debe ser entre 0 y 100...")]
+        public int Age { get; set; }
+
+        [DisplayName("Trabajo")]
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "El TRABAJO debe tener entre 5 y 20 caractéres...")] /* Establece la longitud del campo */
+        public string Job { get; set; }
+
+    }
+}
