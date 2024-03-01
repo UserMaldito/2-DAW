fc893689195385c4e7e7e3efc133890488c18625
Aday <adaymarfil89@gmail.com>
2024-03-01 12:52:02 +0200

    Inicio sin Persistencia

---
+++ DSW/.NET/MVC/Sin Persistencia/Los Simpson Con Amnesia/Los Simpson Con Amnesia/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Los_Simpson_Con_Amnesia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Los_Simpson_Con_Amnesia.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DSW/.NET/MVC/Sin Persistencia/Los Simpson Con Amnesia/Los Simpson Con Amnesia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Los_Simpson_Con_Amnesia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
